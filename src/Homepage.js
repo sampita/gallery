173e10dbb3739ba7d660a5c57fecff1c36c51ac9
Sam Pita <samantha.pita@tuftandneedle.com>
2021-12-30 20:42:18 -0500

    setup homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './Homepage.css';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
+function Homepage() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="site-title">Gallery</h1>
+      <h3>A photo gallery to remember all the good times.</h3>
+      <Navbar />
     </div>
   );
 }
 
-export default App;
+export default Homepage;
